54ace5f2a7259b55d612e5e0c361da1cf66ad5e0
AkiraMatsumoto <95224877+AkiraMatsumoto-github@users.noreply.github.com>
2021-12-11 17:43:16 +0900

    Create memoDetailsScreen

--- App.jsx
+++ App.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
-import AppBar from './src/components/AppBar';
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/components/CircleButton';
+
 // eslint-disable-next-line react/function-component-definition
 export default function App() {
   return (
-  <View style={styles.container}>
-
-    <AppBar />
-    <MemoList />
-    <CircleButton>+</CircleButton>
-  </View>
+    <MemoDetailScreen />
 
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F4F8',
-  },
-});
--- src/components/CircleButton.jsx
+++ src/components/CircleButton.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { string } from 'prop-types';
+import { string, shape } from 'prop-types';
 
 export default function CircleButton(props){
-	const { children } = props;
+	const { children, style } = props;
  	return(
-	<View style={styles.circleButton}>
-		<Text style={styles.circleButtonLabel}>{children}</Text>
+	<View style={[styles.circleButton, style]}>
+  <Text style={styles.circleButtonLabel}>{children}</Text>
 	</View>
 		)
 };
 
 CircleButton.propTypes = {
   children: string.isRequired,
+  shape: shape(),
 };
+CircleButton.defaultProps = {
+  style: null,
+}
 
 const styles = StyleSheet.create({
   circleButton: {
