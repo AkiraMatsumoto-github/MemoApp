eea4f12c770ba1b68a6153768fa203e8beb21fae
AkiraMatsumoto <95224877+AkiraMatsumoto-github@users.noreply.github.com>
2021-12-18 20:33:49 +0900

    Create LogInScreen and SignUpScreen

--- App.jsx
+++ App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-//import MemoEditScreen from './src/screens/MemoEditScreen';
-//import MemoListScreen from './src/screens/MemoListScreen';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SinUpScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 // eslint-disable-next-line react/function-component-definition
 export default function App() {
   return (
-    <MemoCreateScreen />
+    <SignUpScreen />
   );
 }
